914b57c2590b59fb0c816c60144bd2e0d838a276
Vlad Mashurin <27vl@mail.ru>
2019-09-04 12:48:22 +0300

    Added models.

--- src/main/java/com/lapushki/chat/client/Connection.java
+++ src/main/java/com/lapushki/chat/client/Connection.java
@@ -1,20 +1,24 @@
 package com.lapushki.chat.client;
 
 import com.google.gson.Gson;
+import com.lapushki.chat.model.RequestMessage;
+import com.lapushki.chat.model.ResponseMessage;
 import com.lapushki.chat.server.ConnectionListener;
 
 import java.io.*;
 import java.net.Socket;
 
-public class Connection {
+public class Connection implements ConnectionListener {
     private final Socket socket;
     private final Thread thread;
     private final ConnectionListener listener;
     private final BufferedReader in;
     private final BufferedWriter out;
+    private final Gson gson;
 
-    Connection(ConnectionListener listener, Socket socket) throws IOException {
-        this.listener = listener;
+    Connection(Socket socket) throws IOException {
+        listener = this;
+        gson = new Gson();
         this.socket = socket;
         in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
         out = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
@@ -25,8 +29,9 @@ public class Connection {
     private void onMessageReceive (){
         try {
             while (!thread.isInterrupted()) {
-                String msg = in.readLine();
-                listener.onReceivedMessage(msg);
+                String jsonResponse = in.readLine();
+                ResponseMessage responseMessage = gson.fromJson(jsonResponse, ResponseMessage.class);
+                listener.onReceivedMessage(responseMessage.toString());
             }
         } catch (IOException ex) {
             listener.onException(ex);
@@ -35,9 +40,8 @@ public class Connection {
         }
     }
 
-    public synchronized void sendMessage(Message msg) {
+    public synchronized void sendMessage(RequestMessage msg) {
         try {
-            Gson gson = new Gson();
             out.write(gson.toJson(msg));
             out.flush();
         } catch (IOException e) {
@@ -55,5 +59,31 @@ public class Connection {
         } catch (IOException e) {
             listener.onException(e);
         }
+    }
+
+    RequestMessage formMessageObject(String in) {
+        String command = in.substring(0, in.indexOf(" "));
+        String message = in.substring(in.indexOf(" ") + 1);
+        return new RequestMessage(command, message);
+    }
+
+    @Override
+    public void onReceivedMessage(String message) {
+        printMessage(message);
+    }
+
+    @Override
+    public void onDisconnect() {
+        printMessage("Connection closed! Have a good day!");
+    }
+
+    @Override
+    public void onException(Exception ex) {
+        printMessage(ex.getMessage());
+
+    }
+
+    void printMessage(String message) {
+        System.out.println(message);
     }
 }
---
+++ src/main/java/com/lapushki/chat/model/ResponseMessage.java
@@ -0,0 +1,18 @@
+package com.lapushki.chat.model;
+
+public class ResponseMessage {
+    public String status;
+    public String message;
+    public String time;
+
+    public ResponseMessage(String status, String message, String time) {
+        this.status = status;
+        this.message = message;
+        this.time = time;
+    }
+
+    @Override
+    public String toString() {
+        return status.equals("OK") ? String.format("[%s] %s", time, message) : String.format("[ERROR] %s", message);
+    }
+}
