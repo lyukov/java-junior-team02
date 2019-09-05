96d7509c63496101aaa9727585874a9004fa34cf
Elena.Tokareva <freesia24.11@gmail.com>
2019-09-05 16:01:31 +0300

    fix Parser tests

--- src/main/java/com/lapushki/chat/db/Parser.java
+++ src/main/java/com/lapushki/chat/db/Parser.java
@@ -40,6 +40,12 @@ public class Parser {
                     else
                         throw new IOException("Wrong parameter!");
                 }
+                if (url == null || url.isEmpty() ||
+                        user == null || user.isEmpty() ||
+                        password == null || password.isEmpty() ||
+                        database == null || database.isEmpty() ||
+                        table == null || table.isEmpty())
+                    useDefaultConfig();
             } catch (IOException e) {
                 e.printStackTrace();
                 useDefaultConfig();
